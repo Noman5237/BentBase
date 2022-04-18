d2da1ae99fe3bae21857bd4a3344fa4f63e1ddab
Noman5237 <anonyman637@gmail.com>
2022-04-18 08:01:00 +0600

    fixed order id bug

--- database/migrate/3.0.sql
+++ database/migrate/3.0.sql
@@ -4,28 +4,30 @@ Create table Review(
     seller_review varchar2(1000),
     gig_rating INT,
     seller_rating INT,
-    constraint fk_review_order_id foreign key (order_id) references order_info (order_id)
+    constraint fk_review_order_id foreign key (order_id) references order_info (id)
 );
 
-create or replace FUNCTION totalRating (input_email varchar2 )
-RETURN number IS 
-   total number(5) := 0;
-   n number(5) := 0;
-   rating Review.seller_rating%type;
-   order_no order_info.order_id%type;
-   seller_email order_info.seller_email%type;
-   CURSOR c_sellers IS 
-   (SELECT order_id, seller_email FROM order_info where input_email =  order_info.seller_email); 
-BEGIN 
-   OPEN c_sellers; 
-   LOOP 
-   FETCH c_sellers into order_no, seller_email; 
-      EXIT WHEN c_sellers%notfound; 
-      SELECT seller_rating into rating from REVIEW where order_no=order_id;
-      total := total + rating;
-      n := n + 1;
-   END LOOP; 
-   CLOSE c_sellers;
-   rating := total / n;
-   RETURN rating; 
-END; 
+create or replace function totalRating(input_email varchar2)
+    return number is
+    total        number(5) := 0;
+    n            number(5) := 0;
+    rating       review.seller_rating%type;
+    order_no     order_info.id%type;
+    seller_email order_info.seller_email%type;
+    cursor c_sellers is
+        (select id, seller_email
+         from order_info
+         where input_email = order_info.seller_email);
+begin
+    open c_sellers;
+    loop
+        fetch c_sellers into order_no, seller_email;
+        exit when c_sellers%notfound;
+        select seller_rating into rating from review where order_no = order_id;
+        total := total + rating;
+        n := n + 1;
+    end loop;
+    close c_sellers;
+    rating := total / n;
+    return rating;
+end;
