25f52a09321950bc46a99084416f8f34a7d0a83b
Noman5237 <anonyman637@gmail.com>
2022-04-27 14:40:07 +0600

    review class with feature notes

--- backend/src/main/java/com/bentbase/backend/review/Review.java
+++ backend/src/main/java/com/bentbase/backend/review/Review.java
@@ -1,11 +1,9 @@
 package com.bentbase.backend.review;
 
-import com.bentbase.backend.user.User;
-
 public class Review {
 	
 	private String id;
 	private float rating;
 	private String comment;
-	private User reviewer;
+	private String reviewer;
 }
