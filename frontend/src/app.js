8d5e13042e078caeba2f1086d665212769855d2f
Tanmoy-SWE <ahsanhabib3@iut-dhaka.edu>
2022-04-27 14:45:11 +0600

    Gig page updated

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Home as SellerHome } from './seller';
+import Gigs from './seller/gigs.component';
 import Profile from './seller/profile.component';
 
 const App = () => (
 	<div className="App">
-		<Profile/>
+		<Routes>
+			<Route path="" element={<Profile/>} />
+			<Route path="/Gigs" element={<Gigs/>} />
+		</Routes>
 	</div>
 );
 
